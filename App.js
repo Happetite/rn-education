31b8436061fe79dd18b2aecf8030b507abc1e805
Jaejin <jakejaejin@naver.com>
2022-02-16 00:27:34 +0900

    4회차 수업
    4회차 수업

--- App.js
+++ App.js
@@ -1,20 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  SafeAreaView,
+  Modal
+} from "react-native";
+import React, { useState } from "react";
+import { TodoCard } from "./components/TodoCard";
+import {useFonts} from 'expo-font';
+//styling with flexbox, shadows, borderRadius check
+//컴포넌트화 하기 check
+//expo fonts review
+//javascript async await
+  
+//useWindowDimensions, Dimensions
+//expo screen orientation 넘어가기
+//platform API and platform specific code 
+
+
+//react navigation
+
+//SafeAreaView
+//KeyboardAvoidingView
+
+//modal <- 얘의 사용을 비추
 
 export default function App() {
+  
+  const [loaded]=useFonts({
+    customFont1: require('./assets/OpenSans-Regular.ttf')
+  })
+  const [text, setText] = useState();
+  const [todos, setTodos] = useState([]);
+  const [isModalVisible, setIsModalVisible]=useState(false);
+
+  const handleTextChange = (value) => {
+    setText(value);
+  };
+
+  const addTodo = () => {
+
+    setTodos((prev) => {
+      return [...prev, { key: text, content: text }];
+    });
+    setText("")
+  };
+  
+  // const deleteTodo=()=>{
+  //   setTodos((prev)=>{
+  //     return prev.filter((todo)=>{return todo.key!=text});
+  //   })
+  // }
+  const deleteTodo=(string)=>{
+    setTodos((prev)=>{
+      return prev.filter((todo)=>{return todo.key!=string});
+    })
+  }
+
+  
+  if(!loaded){
+    return null;
+  }
+  const [page,setPage]=useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{ padding: 50 }}>
+      {/* 추가버튼과, 입력창 */}
+
+      {/* <TextInput
+        placeholder="할 일을 적으세요"
+        value={text}
+        onChangeText={handleTextChange}
+      /> */}
+      <View>
+      <Button title={"추가"} onPress={()=>{setIsModalVisible(true)}} />
+      </View>
+      {/* 데이터를 리스트로 제공하는 형태가 나와야 함 */}
+
+      {/*<ScrollView >
+        {todos.map((item)=>{
+        return(
+          <View>
+            <Text>
+              {item.content}
+            </Text>
+          </View>
+        )
+        })}
+      </ScrollView> */}
+      <FlatList
+        data={todos}
+        contentContainerStyle={{ alignItems:"center",}}
+        renderItem={(itemData) => {
+          return (
+            <View style={{marginBottom:5}}>
+              <TodoCard onPress={()=>{deleteTodo(itemData.item.content)}} 
+              index={itemData.index} 
+              content={itemData.item.content}/>
+            </View>
+          );
+        }}
+      />
+      {isModalVisible?
+      <View style={{position:"absolute",top:100,backgroundColor:"white", alignSelf:"center",height:600,width:300,borderRadius:25,borderWidth:2}}>
+        <TextInput
+        placeholder="할 일을 적으세요"
+        value={text}
+        onChangeText={handleTextChange}
+        />
+        <Button title={"close"} onPress={()=>{setIsModalVisible(false)}}/>
+        <Button title={"저장"} onPress={()=>{addTodo(); setIsModalVisible(false)}}/>
+      </View>
+      :null}
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/TodoCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View,Text, TouchableOpacity, StyleSheet ,Dimensions, useWindowDimensions, Platform} from 'react-native';
+import { borderRightColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+//expo-fonts
+export const TodoCard=(props)=>{
+    //prop->todo에 들어갈 내용과, 인덱스가 주어지면
+    //그거를 예쁘게 나타내는 카드
+    //터치가 가능한 component->onPress 함수를 실행해야함->prop으로 받아야 함
+    // const width=Dimensions.get("window").width;
+    // const height=Dimensions.get("window").height;
+    // Dimensions.addEventListener()
+    const width=useWindowDimensions().width;
+    const height=useWindowDimensions().height;
+    //android 오류가 있음 reliable
+    return(
+        <TouchableOpacity  onPress={props.onPress}>
+              <View style={styles.card}>
+                <View>
+                    <Text style={{fontFamily:"customFont1"}} >
+                    {props.index}
+                    </Text>
+                </View>
+                <View>
+                    <Text>
+                    {props.content}
+                    </Text>
+                </View>
+              </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles=StyleSheet.create({
+
+    card:{
+        backgroundColor:"white",
+        height:50,
+        width:300,
+        borderRadius:15,
+        elevation:1, //android->ios에서는 작동 x
+        shadowColor:"black" ,
+        shadowRadius:1 , //ios->android 작동 x
+        alignItems:"center",
+        
+    }
+})
